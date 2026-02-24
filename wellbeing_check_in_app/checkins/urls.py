abc192c3ea1dfb353fb5da8eb85b5ff71a96bbc2
Maksym Pehashev <pegashev95@gmail.com>
2026-02-24 19:59:00 +0000

    Add check-in delete view with confirmation page and ownership protection

--- wellbeing_check_in_app/checkins/urls.py
+++ wellbeing_check_in_app/checkins/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.checkin_list, name="checkin_list"),
     path("new/", views.checkin_create, name="checkin_create"),
     path("<int:pk>/edit/", views.checkin_update, name="checkin_update"),
+    path("<int:pk>/delete/", views.checkin_delete, name="checkin_delete"),
 ]
