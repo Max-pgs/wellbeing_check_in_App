04d61ca78bae412a0be4372cc0861166d3010b60
Maksym Pehashev <pegashev95@gmail.com>
2026-02-24 19:26:38 +0000

    Add Django auth URL routes under /accounts/

--- wellbeing_check_in_app/checkins/urls.py
+++ wellbeing_check_in_app/checkins/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("checkins/", views.checkin_list, name="checkin_list"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("checkins/new/", views.checkin_create, name="checkin_create"),
 ]
